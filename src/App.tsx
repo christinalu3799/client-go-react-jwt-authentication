914cb6fd86f8ffdab0df2669b308f865a5d0c66c
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-12-14 16:36:34 -0500

    created nav component

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Home</a>
         
-                <div>
-                    <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Login</a>
-                        </li>
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Register</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Nav />
         <main className="form-signin">
             <Login />
         </main>
