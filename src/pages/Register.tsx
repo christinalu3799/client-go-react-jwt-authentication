5278dfe7bfd6d278a0d9399f4ef9bd88de9e7e8f
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-12-14 16:50:19 -0500

    created register page form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+    const [name, setName] = useState('')
+    return (
+        <div>
+            <form>
+                <h1 className="h3 mb-3 fw-normal">Please register new account</h1>
+
+                <input className="form-control" placeholder="Name" required/>
+
+                <input type="email" className="form-control" placeholder="Email address" required/>
+
+                <input type="password" className="form-control" placeholder="Password" required/>
+
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default Register
