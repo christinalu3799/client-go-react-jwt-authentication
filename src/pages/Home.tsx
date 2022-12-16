e90890e38959adbb006a1f220c8d1721bb7174c4
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-12-16 13:36:19 -0500

    able to render username on screen after login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+    const [name, setName] = useState('')
     // since useEffect doesn't accept async functions, we have to do it like this
     useEffect(() => {(
         async () => {
-            await fetch('http://localhost:8000/api/user', {
+            const response = await fetch('http://localhost:8000/api/user', {
                 method: 'GET',
                 credentials: 'include'
             })
+            const content = await response.json()
+            setName(content.name)
         }
+
     )()})
     return (
-        <div>Home</div>
+        <div>{name ? 'Hi ' + name : 'You are not logged in.'}</div>
     )
 }
 
