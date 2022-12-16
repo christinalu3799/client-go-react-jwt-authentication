823bccc32db9ac559895a0800619fd8560c36a0e
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-12-16 12:52:40 -0500

    able to get user information after login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+    // since useEffect doesn't accept async functions, we have to do it like this
+    useEffect(() => {(
+        async () => {
+            await fetch('http://localhost:8000/api/user', {
+                method: 'GET',
+                credentials: 'include'
+            })
+        }
+    )()})
     return (
         <div>Home</div>
     )
